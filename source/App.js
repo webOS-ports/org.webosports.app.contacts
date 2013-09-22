709f370adb9046892482891a8b02b9756192e8af
Whitebee <whitebee@outlook.com>
2013-09-23 00:54:20 +0300

    basic layout

--- source/App.js
+++ source/App.js
@@ -1,17 +1,60 @@
+//Contacts List
 enyo.kind({
-	name: "App",
-	kind: "FittableRows",
-	fit: true,
-	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
-		{kind: "enyo.Scroller", fit: true, components: [
-			{name: "main", classes: "nice-padding", allowHtml: true}
-		]},
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
-		]}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
-	}
+    name: "ContactItem",
+    components: [
+        { name: "profilePicture", kind: "Image" },
+        { name: "name" }
+    ]
 });
+
+
+enyo.kind({
+    name: "ContactsList",
+    kind: "enyo.List",
+    components: [
+        { name: "item", kind: "ContactItem" }
+    ]
+});
+
+
+enyo.kind({
+    name: "TabsControl",
+    kind: "FittableRows",
+    published: {
+        panes: ""
+    },
+    components: [
+        { kind: "enyo.RadioGroup" },
+        { kind: "enyo.Panels", arrangerKind: "enyo.LeftRightArranger", margin: 0, fit: true }
+    ]
+});
+
+
+enyo.kind({
+    name: "ContactsBar",
+    kind: "FittableRows",
+    components: [
+        { kind: "TabsControl", fit: true, panes: [
+            
+            { name: "main", description: "All", kind: "FittableRows", components: [
+                {kind: "onyx.InputDecorator", components: [
+				    {kind: "onyx.Input", placeholder: "Search"},
+				    {kind: "Image", src: "assets/search-icon.png"}
+                ]},
+                { kind: "ContactsList" , fit: true}
+            ]},
+            
+            { name: "favourites", description: "Favourites", components: [
+                { kind: "ContactsList" }
+            ]}
+        ]},
+        
+        { name: "BottomToolbar", kind: "onyx.Toolbar", components: [
+            { kind: "onyx.Button", content: "Add Contact"}
+        ]}
+    ]
+});
+
+//Contact Details
+
+//App
