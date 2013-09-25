dff2d42f13320d1fb4a12db2fc2cb4281b05dc76
Whitebee <whitebee@outlook.com>
2013-09-25 14:55:15 +0300

    improve layout and apply styles

--- source/App.js
+++ source/App.js
@@ -1,9 +1,20 @@
 //Contacts List
 enyo.kind({
     name: "ContactItem",
+    published: {
+        icon: {
+            src: "",
+            showing: false
+        }
+    },
     components: [
-        { name: "profilePicture", kind: "Image" },
-        { name: "name" }
+        //{ name: "profilePicture", kind: "Image", src: "assets/bg_icon_img.png" },
+        { classes: "icon", components: [
+            { name: "photo", classes: "img" },
+            { classes: "mask"}
+        ] },
+        { name: "name", content: "Name Name", classes: "name" },
+        { name: "specialIcon", kind: "onyx.Icon", showing: false }
     ]
 });
 
@@ -11,8 +22,10 @@ enyo.kind({
 enyo.kind({
     name: "ContactsList",
     kind: "enyo.List",
+    count: 12,
+    item: "item",
     components: [
-        { name: "item", kind: "ContactItem" }
+        { name: "item", kind: "ContactItem", classes: "contacts-item" }
     ]
 });
 
@@ -20,13 +33,46 @@ enyo.kind({
 enyo.kind({
     name: "TabsControl",
     kind: "FittableRows",
+    style: "text-align:center;",
     published: {
         panes: ""
     },
     components: [
-        { kind: "enyo.RadioGroup" },
-        { kind: "enyo.Panels", arrangerKind: "enyo.LeftRightArranger", margin: 0, fit: true }
-    ]
+        { name: "tabsToolbar", kind: "onyx.Toolbar", components: [
+            { name: "tabs", kind: "onyx.RadioGroup", controlClasses: "onyx-tabbutton", onActivate: "paneChange", components: [
+                { content: "All", index: 0, active: true },
+                { content: "Favourites", index: 1 }
+            ]}
+        ]},
+        { name: "panes", kind: "enyo.Panels", arrangerKind: "enyo.LeftRightArranger", events: { onPaneChanged: "" }, margin: 0, fit: true, components: [
+            { name: "main", description: "All", kind: "FittableRows", classes: "contacts-list", components: [
+                { kind: "onyx.InputDecorator", classes: "contacts-search", components: [
+				    { kind: "onyx.Input", placeholder: "Search" },
+				    { kind: "Image", src: "assets/search-input.png" }
+                ]},
+                { kind: "ContactsList" , fit: true}
+            ]},
+            
+            { name: "favourites", description: "Favourites", components: [
+                { kind: "ContactsList" }
+            ]}
+        ],  
+    /*indexChanged: function(oldValue) {
+        //this.doPaneChanged(this.getIndex());
+    }*/
+        }
+    ],
+    
+    paneChange: function(inSender, inEvent) {
+        if (inEvent.originator.getActive()) {
+            //Is this okay, without index being published?
+            this.$.panes.setIndex(inEvent.originator.index);
+        }
+    },
+    
+    tabChange: function(newIndex) {
+        this.$.tabs.$[newIndex].setActive(true);
+    }
 });
 
 
@@ -47,14 +93,83 @@ enyo.kind({
             { name: "favourites", description: "Favourites", components: [
                 { kind: "ContactsList" }
             ]}
-        ]},
+        ]}
         
+    ]
+});
+
+//Contact Details
+enyo.kind({
+    name: "Detail",
+    components: [
+        { tag: "p", name: "value", classes: "value", content: "joe@dude.com" },
+        { tag: "p", name: "label", classes: "label", content: "Work Email" }
+    ]
+});
+
+
+enyo.kind({
+    name: "Header",
+    kind: "enyo.FittableRows",
+    classes: "header",
+    components: [
+        { kind: "enyo.FittableColumns", fit: true, components: [
+            //{ name: "profilePicture", kind: "enyo.Image" },
+            { classes: "avatar", components: [
+                { name: "photo", classes: "img" },
+                { classes: "mask"}
+            ]},
+            { classes: "nameinfo", fit: true, components: [
+                { name: "name", classes: "name", content: "Joey Joe" },
+                { name: "nickname", classes: "nickname", content: "JoJo" },
+                { name: "job", classes: "position", content: "Joer, Job" }
+            ]},
+            { kind: "enyo.FittableRows", style: "text-align: right", components: [
+                { name: "favourite", kind: "onyx.ToggleIconButton", classes: "favorite", src: "assets/bg_details_favorite.png" },
+                { fit: true },
+                { name: "profilesButton", classes: "profiles-button", kind: "onyx.Button", ontap: "openProfilesList", content: "2 Profiles" }
+            ]}
+        ]},
+        { name: "profiles", kind: "onyx.Drawer", open: false, components: [
+            { name: "profilesList", kind: "enyo.List", components: [
+                { name: "profile", kind: "ContactItem" }
+            ]},
+            { name: "linkProfile", content: "Link More Profiles..." }
+        ]}
+    ],
+    openProfilesList: function () {
+        this.$.profiles.setOpen(!this.$.profiles.open);
+        this.$.profilesButton.addRemoveClass("active", this.$.profiles.open);
+    }
+});
+
+enyo.kind({
+    name: "ContactDetails",
+    kind: "enyo.Scroller",
+    classes: "details",
+    components: [ { name: "container", classes: "container", components: [
+        { name: "content", classes: "content", components: [
+            { name: "header", kind: "Header" },
+            { name: "details", kind: "enyo.List", classes: "group", count: 4, components: [
+                { name: "contactDetail", kind: "Detail", classes: "contacts-item" }
+            ]}
+        ]}
+    ]}
+    ]
+});
+
+
+//App
+enyo.kind({
+    name: "App",
+    kind: "FittableRows",
+    components: [
+        { name: "main", kind: "enyo.Panels", arrangerKind: "enyo.CollapsingArranger", draggable: false, fit: true, components: [
+            { name: "bar", kind: "ContactsBar" },
+            { name: "details", kind: "ContactDetails", fit: true }
+        ]},
         { name: "BottomToolbar", kind: "onyx.Toolbar", components: [
             { kind: "onyx.Button", content: "Add Contact"}
         ]}
     ]
 });
-
-//Contact Details
-
-//App
