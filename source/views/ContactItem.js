31fffe3f0a6f1337094d70ed5a704c6d437c6c22
P. Douglas Reeder <reeder.29@gmail.com>
2014-11-21 15:21:17 -0500

    Contact name in list uses available width

--- source/views/ContactItem.js
+++ source/views/ContactItem.js
@@ -11,20 +11,22 @@ enyo.kind({
         // Quotes are needed to work around a bug in Black Eye when using sizing "cover".
         // The should not be needed (and are not needed in Safari 7.1).
         {from: ".model.listPhoto", to: ".$.photo.src", transform: function(path) {return "'" + (path || "assets/bg_icon_img.png") + "'" ;}},
-        {from: ".model.favorite", to: ".$.favIcon.showing"}
+        {from: ".model.favorite", to: ".$.favCell.showing"}
     ],
     components: [
         //{ name: "profilePicture", kind: "Image", src: "assets/bg_icon_img.png" },
         { classes: "icon", components: [
             {
                 name: "photo",
-                classes: "img",
+                classes: "list-img",
                 kind: "enyo.Image",
                 sizing: "cover"
             },
-            { classes: "mask"}
+            { classes: "list-mask"}
         ] },
         { name: "name", content: "Name Name", classes: "name" },
-        { name: "favIcon", classes: "favorite", kind: "onyx.Icon", showing: false }
+        { name: "favCell", classes: "favorite-cell", showing: false, components: [
+            { name: "favIcon", classes: "favorite", kind: "onyx.Icon" }
+        ]}
     ]
 });
