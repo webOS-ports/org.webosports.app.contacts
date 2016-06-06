1d6b19e2a31a4a2dbcdecdddec9d37ada930847e
P. Douglas Reeder <reeder.29@gmail.com>
2016-06-05 23:08:52 -0400

    In contact details pane, tapping on a phone number, email, IM, URL, address, birthday or anniversary attempts to open the apropriate app. Tapping on a relation searches for that name in Contacts.
    Open-WebOS-DCO-1.0-Signed-Off-By: Doug Reeder reeder.29@gmail.com

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -62,7 +62,14 @@ module.exports = kind({
                                 }
                             ]
                         },
-                        { name: "contactDetails", kind: ContactDetails, fit: true, onPersonChanged: "savePerson", onEdit: "showEdit" }
+                        {
+							name: "contactDetails",
+							kind: ContactDetails,
+							fit: true,
+							onPersonChanged: "savePerson",
+							onEdit: "showEdit",
+							onAlterSearch: 'alterSearch'
+						}
                     ]
                 }
             ]
@@ -289,6 +296,13 @@ module.exports = kind({
 			});
 			oldPerson.set(attrName, attr);
 			changed = true;
+		}
+	},
+
+	alterSearch: function (inSender, inEvent) {
+		this.$.contactsBar.alterSearch(inEvent.searchText);
+		if (Panels.isScreenNarrow() && this.$.main.get("index") > 0) {
+			this.$.main.setIndex(0);
 		}
 	},
     
